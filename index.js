89a8d6ba835511de4cf47031d14f8254df724f92
jadraza <bpedrazaj@uc.cl>
2025-03-28 14:59:18 -0300

    [FUNCTIONS] added deleteRow and deleteColumn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { read, write, insertRow, insertColumn, swapColumns } = require('./utils');
+const { read, write, insertRow, insertColumn, swapColumns, deleteRow, deleteColumn } = require('./utils');
 
 const filePath = path.join(__dirname, 'test.csv');
 let csvData = read(filePath);
@@ -9,5 +9,7 @@ const columnToInsert = ["1", "2", "3", "4"];
 csvData = insertRow(csvData, 2, rowToInsert);
 csvData = insertColumn(csvData, 3, columnToInsert);
 csvData = swapColumns(csvData, 1, 3);
+csvData = deleteRow(csvData, 1);
+csvData = deleteColumn(csvData, 2);
 
 write(filePath, csvData);
