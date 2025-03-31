7978969085e8a44db8e85af20e5f565818a1b59b
MARTIN JARA <martinjf@uc.cl>
2025-03-31 02:29:40 -0300

    update README

---
+++ csv-app/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    base: '/2025-1-s1-g13-t1/',
+    plugins: [
+      tailwindcss(),
+    ],
+  });
